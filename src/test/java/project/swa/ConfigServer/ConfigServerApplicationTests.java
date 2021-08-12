5ab22c43ca5aa59f62cdbfbdcd4fb0f5a7fcc52a
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-08-12 02:49:08 -0500

    created ConfigServer and setup the configuration for other microservice with spring_profile_name : native

---
+++ src/test/java/project/swa/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.swa.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
